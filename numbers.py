08d624781a81d5d2291bee372f927abaae5fa4d4
legi setiawan <legi.setiawan@gmail.com>
2024-01-08 22:04:55 +0700

    enulis code number,float,complex, list

--- numbers.py
+++ numbers.py
@@ -1,1 +1,13 @@
+# Number Int bilangan bulat positif dan negatif
+print(1)
+#Bilangan bulat boolean mengembalikan true atau false
+#number float
+print(1.5) # dipisah dengan titik
+#Bilangan Complex adalah gabungan 2 bilangan int dengan operator matematika(bilangan int denga huruf) tidak boleh ada spasi.
+print("1+2js")
 
+#type data urutan list, tuple, range.
+list = ["a","b","c"]
+print(list)
+print(list[0]) #mengambil bilangan pertama
+print(list[2]) #mengambil bilangan keduas
