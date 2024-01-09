b575cf491a2e44e436b03415cfdda4ba3e9462b1
legi setiawan <legi.setiawan@gmail.com>
2024-01-10 01:19:46 +0700

    menambah code operator logika dan mengubah code hasi pembagian

--- operator.py
+++ operator.py
@@ -2,8 +2,7 @@
 x = 2
 y = 10
 print(f"hasil bagi = {y} / {x}" )
-bagi = int(y/x) # conversi bilangan float ke int
-print(bagi)
+print(y//x) # agar hasil pembagian menjadi bilangan bulat gunakan //
 print(f"hasil kali = {y} * {x}" )
 kali = int(y*x) # conversi bilangan float ke int
 print(kali)
@@ -17,4 +16,14 @@ print(f"hasil modulus = {y} % {x}" )
 kali = int(y%x) # conversi bilangan float ke int
 print(kali)
 
-
+#  Operator Logika
+print("AND:")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print("OR:")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
