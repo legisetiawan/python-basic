281c6ddca4516ff47eb1428e456c7bf36c5fd4b4
legi setiawan <legi.setiawan@gmail.com>
2024-01-10 04:45:40 +0700

    menambah file ifelse nulis code

---
+++ ifelse.py
@@ -0,0 +1,7 @@
+# if kondisi jika true code di eksekusi jika false jalankan else
+nilai = 68
+b = 70
+if nilai > b :
+    print(f"Nilai anda {nilai} besar  dari standar B {b} :Baik")
+else :
+    print(f"Nilai anda {nilai}  kurang dari standar B {b} : Kurang baik")
