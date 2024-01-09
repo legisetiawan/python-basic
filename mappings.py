01851ac6180f7daddbd2b44e5cd3f419fc640065
legi setiawan <legi.setiawan@gmail.com>
2024-01-09 19:51:58 +0700

    menambah file mappings jenis dict menulis code

---
+++ mappings.py
@@ -0,0 +1,12 @@
+    # kumpulan index terbatas yg di index oleh kumpulan index arbitrer, jenis ada satu yaitu DICT data terdiri dari key dan value 
+map = {
+        "brand" : "honda",
+        "model" : "Hornet CB",
+        "year" : 1987
+        }
+print(map["brand"])
+print(map["year"])
+print("type data:", type(map))
+
+
+
