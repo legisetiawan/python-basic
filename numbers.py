be586c693be45e020872d4d4c3b96883d77a00c2
legi setiawan <legi.setiawan@gmail.com>
2024-01-09 14:31:46 +0700

    menambah file sequences dan edit code

--- numbers.py
+++ numbers.py
@@ -5,24 +5,3 @@ print(1)
 print(1.5) # dipisah dengan titik
 #Bilangan Complex adalah gabungan 2 bilangan int dengan operator matematika(bilangan int denga huruf) tidak boleh ada spasi.
 print("1+2js")
-
-#type data urutan list, tuple, range.
-list = ["a","b","c"]
-print(list)
-print(list[0]) #mengambil bilangan pertama
-print(list[2]) #mengambil bilangan keduas
-
-#Type data tuple type data yg tidak bisa di ubah
-tuple =("ani","budi")
-print(tuple)
-print(tuple[0])
-# perbedaan list dengan tuple data tuple tidak bisa di ubah
-list = ["a""b"]
-        list[0] ="c"
-        print(list)
-tuple = ("a""b")
-tuple[0] = "d"
-print(tuple)
-# type data range
-range = range(1.10)
-print(range)
---
+++ sequences.py
@@ -0,0 +1,23 @@
+#type data urutan list, tuple, range.
+list = ["a","b","c"]
+print(list)
+print('bil pertama:')
+print(list[0]) #mengambil bilangan pertama
+print('bil kedua:')
+print(list[2]) #mengambil bilangan keduas
+
+#Type data tuple type data yg tidak bisa di ubah
+tuple =("ani","budi")
+print(tuple)
+print(tuple[0])
+# perbedaan list dengan tuple data tuple tidak bisa di ubah
+list = ["a""b"]    
+tuple = ("a""b")
+list[0] = "b"
+print(list)
+print(tuple)
+print('list tuple')
+# type data range
+range = range(10)
+print(range)
+print(range[0])
