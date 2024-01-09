f3b371237d10e3f6f64cfde3e34a2e063d06514c
legi setiawan <legi.setiawan@gmail.com>
2024-01-09 07:27:01 +0700

    mengubah code

--- numbers.py
+++ numbers.py
@@ -11,3 +11,18 @@ list = ["a","b","c"]
 print(list)
 print(list[0]) #mengambil bilangan pertama
 print(list[2]) #mengambil bilangan keduas
+
+#Type data tuple type data yg tidak bisa di ubah
+tuple =("ani","budi")
+print(tuple)
+print(tuple[0])
+# perbedaan list dengan tuple data tuple tidak bisa di ubah
+list = ["a""b"]
+        list[0] ="c"
+        print(list)
+tuple = ("a""b")
+tuple[0] = "d"
+print(tuple)
+# type data range
+range = range(1.10)
+print(range)
