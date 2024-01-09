434ccba575125336db4f5b53272f8e7beca71b38
legi setiawan <legi.setiawan@gmail.com>
2024-01-09 19:13:46 +0700

    menambah file set dan menulis code

---
+++ set.py
@@ -0,0 +1,8 @@
+#kumpulan objek unik yg tidak dapat di ubah tidak berurutan dan terbatas, tidak berindex fungsi bawaan len(),add(),remove()
+set = {"ani", "budi"}
+print(set)
+print("jumlah data :")
+print(len(set))
+set.add("nana") # tambah satu data
+print("jumlah data :")
+print(len(set)) # jumlah data
