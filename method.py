a03373a64779ddff4bbea2cd7ec4de5c5985784b
legi setiawan <legi.setiawan@gmail.com>
2024-01-10 23:31:26 +0700

    membuat file method menulis code

---
+++ method.py
@@ -0,0 +1,9 @@
+# method atau fungsi yg berisi code block yg di dapat di gunakan  dlm keadaan tertentu
+nama = []
+
+def get_nama():  
+    for name in nama:
+        print(f"Hai {name} :")
+nama.append("ani")
+nama.append("ana")
+get_nama()
