2f01afd1f1b10dbb5aa0a55012573f17714a6c1a
legi setiawan <legi.setiawan@gmail.com>
2024-01-11 12:24:23 +0700

    menambah file module nulis code

---
+++ module.py
@@ -0,0 +1,10 @@
+# mengimport code dari file-file yg di butuhkan gunakan import nama_file
+import return_value # mengimport semua method
+from return_value import perkalian
+from return_value import penjumlahan
+
+jumlah =penjumlahan(2,4,5)
+kali =perkalian(2,4,5) # tiap bilangan di kali 2(ditentukan) di simpan dan di kalikan dengan bilangan selanjutnya.
+print(jumlah)
+print(kali)
+
--- return_value.py
+++ return_value.py
@@ -4,6 +4,19 @@ def jumlah_list(*list_float):
     for angka in list_float:
         total = total + angka
     return (list_float,total)
-list_float,total = jumlah_list(23.0,23.9,12.9,4.3)
-print(total)
-print(f"Total penjumlahan :{list_float} = {total}") # karna gak bisa mengakses list_float, retun di buat tuple
+
+def penjumlahan(*list_float):
+    total = 0
+    for angka in list_float:
+        total = total + angka
+    return (total)
+
+def perkalian(*list_float):
+    total = 2
+    for angka in list_float:
+        total = total * angka
+    return (total)
+
+# list_float,total = jumlah_list(23.0,23.9,12.9,4.3)
+# print(total)
+# print(f"Total penjumlahan :{list_float} = {total}") # karna gak bisa mengakses list_float, retun di buat tuple
