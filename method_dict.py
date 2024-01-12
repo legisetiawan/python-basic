384e1216ea41690f7e2c7650fac560776184c41c
legi setiawan <legi.setiawan@gmail.com>
2024-01-13 00:29:57 +0700

    menambah file method dict nulis code, nambah kode method set

---
+++ method_dict.py
@@ -0,0 +1,4 @@
+buah = dict([("buah","mangga"),("harga",12000)]) # membuat data dict dari method dict
+sayur = dict(sape=4139, guido=4127, jack=4098) # membuat data dict dengan kata kunci key value sama...????
+print(buah)
+print(sayur)
