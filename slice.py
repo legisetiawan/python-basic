1ea143e8e90f7dfecf3350ac5d5884a35d35d17d
legi setiawan <legi.setiawan@gmail.com>
2024-01-12 23:54:49 +0700

    menambah file slice dan menulis code

---
+++ slice.py
@@ -0,0 +1,13 @@
+a = [1,2,3,4,5,6,7,8,9,10]
+b = [1,2,3,4,5,6,7,8,9,10]
+c = [1,2,3,4,5,6,7,8,9,10]
+d = [1,2,3,4,5,6,7,8,9,10]
+e = [1,2,3,4,5,6,7,8,9,10]
+f = [1,2,3,4,5,6,7,8,9,10]
+
+print(a[2:]) # dari kiri di buang
+print(b[:2])  # dari kiri di ambil
+print(c[2:4]) # diambil dari index 2 dan sebelum index 4
+print(d[2:5:7])
+print(e[2::2]) # mengambil setiap index ke 2 jalan lagi dari index terakhir dua kali ambil bil dan seterusnya.
+print(f[-5:-2]) # membuang index ke 5 dan mengambil index ke 2 kiri.
