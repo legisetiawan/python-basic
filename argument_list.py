a201eddad26038cfba24f4c3d96750ea64b652f2
legi setiawan <legi.setiawan@gmail.com>
2024-01-13 04:11:57 +0700

    menghapus komentar return value dan edit code argument list

--- argument_list.py
+++ argument_list.py
@@ -1,7 +1,35 @@
 # jika kita memiliki argument lebih dari satu, bisa menggunakan list argument sebelum argument *argument= value disimpan paling terakhir jika ada argument lain buat di awal argument
-def jumlah_list(*list_float):
+def penjumlahan(*list_float):
     total = 0.0
     for angka in list_float:
-        total = total + angka
+        total += angka
     print(f"Total penjumlahan : {list_float} = {total}")
-jumlah_list(23.0,23.9,12.9,4.3)
+
+def pengurangan(*list_float):
+    kurang = list_float
+    for angka in list_float:
+        kurang = kurang - angka
+    print(f"Total pengurangan {list_float} = {kurang} ")
+
+
+def perkalian(*list_float):
+    kali = 0.0
+    for angka in list_float:
+        kali * angka
+    return kali
+
+def pembagian(*list_float):
+    bagi = 0.0
+    for angka in list_float:
+        bagi /= angka
+    return bagi
+pengurangan(23,3,2,4)
+kali = perkalian(3,3,2,4)
+bagi = pembagian(23,3,2,3)
+penjumlahan(23.0,23.9,12.9,4.3)
+print("kurang :")
+# print(kurang)
+print("kali :")
+print(kali)
+print("Bagi :")
+print(bagi)
--- return_value.py
+++ return_value.py
@@ -17,6 +17,6 @@ def perkalian(*list_float):
         total = total * angka
     return (total)
 
-# list_float,total = jumlah_list(23.0,23.9,12.9,4.3)
-# print(total)
-# print(f"Total penjumlahan :{list_float} = {total}") # karna gak bisa mengakses list_float, retun di buat tuple
+list_float,total = jumlah_list(23.0,23.9,12.9,4.3)
+print(total)
+print(f"Total penjumlahan :{list_float} = {total}") # karna gak bisa mengakses list_float, retun di buat tuple
