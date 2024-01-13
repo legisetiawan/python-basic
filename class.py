235b1337b458dd70fd65f50dd6c7117d7cf3768c
legi setiawan <legi.setiawan@gmail.com>
2024-01-13 19:18:43 +0700

    mengedit code

--- class.py
+++ class.py
@@ -1,11 +1,19 @@
-class Siswa():
-    """docstring for Siswa."""
-    def __init__(self,name,age,height):
-        self.name = name
-        self.age = age
-        self.height = height
+
+class Murid():
+
+    def __init__(self,nama,umur):
+        self.nama = nama
+        self.umur = umur
+class Siswa(Murid):
+    def __init__(self,nama,):
+        Murid.__init__(self,nama,9)
+class Siswi(Murid):
+    def __init__(self,nama):
+        Murid.__init__(self,nama,8)
         
-kelas_satu = Siswa("andi",7,21) 
-kelas_dua = Siswa("ana",8,24) 
-print(kelas_satu.__dict__)
-print(kelas_dua.__dict__)
+      
+     
+S1 = Siswa("Anto")
+S2 = Siswi("Ana")
+print(f"Nama Siswa :  {S1.nama} Umur : {S1.umur}")
+print(f"Nama Siswi :  {S2.nama} Umur : {S2.umur}")
