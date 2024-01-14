cbd1210e40dbfe831e897a257acd9a1a00666a7e
legi setiawan <legi.setiawan@gmail.com>
2024-01-14 19:47:09 +0700

    menambah file nested list / list bersarang nulis code

---
+++ nested_list.py
@@ -0,0 +1,8 @@
+a = ["andi",12,"l"]
+b = ["anto",14,"l"]
+c = ["anti",10,"p"]
+list = [a,b,c]
+for i in list:
+    print(f"Nama\t: {i[0]}")
+    print(f"Umur\t: {i[1]}")
+    print(f"kel\t: {i[2]}\n")
