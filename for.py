1f81204f7c306e4ba779adbae2ae847473b9a635
legi setiawan <legi.setiawan@gmail.com>
2024-01-14 22:42:29 +0700

    menambahkan code.

--- for.py
+++ for.py
@@ -3,8 +3,16 @@ siswa = {
     "nama":"ani",
     "umur":13,
     "tgl_lahir":19-1-1720}
-for i in siswa:
-    a = siswa[i]
-    print(i,a)
+# for i in siswa:
+#     a = siswa[i]
+#     print(i,a)
+a = 5
+b = 1
+for i in range(a):
+    print("#"*b)
+    b += 1
+for i in range(a):
+    print("#"*b)
+    b -= 1
 
     
