bcdf5ea7aefe578425848056002b1684bbc245e3
legi setiawan <legi.setiawan@gmail.com>
2024-01-14 22:50:44 +0700

    menambah keterangan method copy() di dict.

--- dict.py
+++ dict.py
@@ -1,5 +1,5 @@
 # clear()	Removes all the elements from the dictionary
-# copy()	Returns a copy of the dictionary
+# copy()	Returns a copy of the dictionary JIKA MENGUNAKAN copy() di file utama data tidak berubah....!
 # fromkeys()	Returns a dictionary with the specified keys and value
 # get()	Returns the value of the specified key
 # items()	Returns a list containing a tuple for each key value pair
