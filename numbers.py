f36fd3575a76d942a257fb3c11468dfe475a1502
legi setiawan <legi.setiawan@gmail.com>
2024-01-17 11:31:24 +0700

    menambah file exception error nulis code

--- numbers.py
+++
@@ -1,7 +0,0 @@
-# Number Int bilangan bulat positif dan negatif
-print(1)
-#Bilangan bulat boolean mengembalikan true atau false
-#number float
-print(1.5) # dipisah dengan titik
-#Bilangan Complex adalah gabungan 2 bilangan int dengan operator matematika(bilangan int denga huruf) tidak boleh ada spasi.
-print("1+2js")
