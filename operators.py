18767a4fd2867ad1d9dfc8cd161af8e6f6e7e386
legisetiawan <legi.setiawan@gmail.com>
2024-01-22 10:19:14 +0700

    mengubah nama karna ada import help...?

---
+++ operators.py
@@ -0,0 +1,44 @@
+# Operator matematika
+x = 2
+y = 10
+print("Matematika :")
+print(f"hasil bagi = {y} / {x}" )
+print(y//x) # agar hasil pembagian menjadi bilangan bulat gunakan // (operator khusus)
+
+print(f"hasil kali = {y} * {x}" )
+kali = int(y*x) # conversi bilangan float ke int
+print(kali)
+print(f"hasil tambah = {y} + {x}" )
+kali = int(y+x) # conversi bilangan float ke int
+print(kali)
+print(f"hasil kurang = {y} - {x}" )
+kali = int(y-x) # conversi bilangan float ke int
+print(kali)
+print(f"hasil modulus = {y} % {x}" )
+kali = int(y%x) # % Operator khusus
+print(kali)
+#  Operator Logika
+print("Logika :")
+print("AND:")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print("OR:")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+# Operator perbandingan 2 buah variable kembalian dari perbandingan adalah boolean false and true
+x = 2
+y = 10
+print("Perbandingan :")
+print("2:10 besar dari",x>y)
+print("2:10 kecil dari",x<y)
+print("2:10 kecil sama dari",x<=y)
+print("2:10 besar sama dari",x>=y)
+print("2:10 tidak sama dari",x!=y)
+print("2:10 sama",x==y)
+print("eksponancial pangkat bilangan tertentu :")
+dual = 2**4
+print(dual)
