d91b841efdeb32d47e37651e0a5a513f654c65e5
legisetiawan <legi.setiawan@gmail.com>
2024-01-23 15:15:09 +0700

    menambah code dan pengertianya

--- class_abstract.py
+++ class_abstract.py
@@ -1,15 +1,32 @@
 from abc import ABC, abstractmethod
 # mewarisi method abstact class import dari abc, dan ABC class abtract, abstractmethod decorator methodnya.
 class Button(ABC):
+    def __init__(self,set_link):
+        self.link = set_link
     @abstractmethod
     def click(self):
+        pass
+    
+    @property
+    @abstractmethod
+    def link(self):
         pass
     
 class PressButton(Button):
     
     def click(self):
-        print("Press Button click")
+        print("Got to link:{}".format(self.link))
         
-button = PressButton()
+    @Button.link.setter
+    def link(self,input):
+        self.__link = input
+        
+    @link.getter
+    def link(self):
+        return self.__link
+        
+        
+        
+button = PressButton("www.google.com")
 button.click()
-help(button)
+# help(button)
